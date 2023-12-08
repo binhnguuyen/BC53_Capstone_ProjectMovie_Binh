73d68209e154fabf7273414ad15896c75bfd8e48
binhnguyen <leechiupin@gmail.com>
2023-12-08 23:51:37 +0900

    Thêm CSS cho ghế thường và ghế VIP

--- src/modules/booking/Booking.jsx
+++ src/modules/booking/Booking.jsx
@@ -5,6 +5,8 @@ import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import style from "./style.css";
 import { red } from '@mui/material/colors';
+import cn from "classnames"
+
 // import base from "../../styles/base.css"
 
 const Booking = () => {
@@ -40,167 +42,178 @@ const Booking = () => {
 
 
     return (
-        <div>
-            <Container maxWidth="xl">
-                <Grid container spacing={2}>
-                    <Grid
-                        item
-                        xs={7}
-                        lg={7}
-                        xl={7}
-                        alignItems={"center"}
-                    >
-                        <Grid container spacing={3}>
-                            {chairList.map((ghe) => {
-                                return (
-                                    <Grid
-                                        item
-                                        key={ghe.tenGhe}
-                                        xs={3}
-                                        sm={2}
-                                        md={1}
-                                        lg={1}
-                                    >
-                                        <Typography className="ghe">
+        <Container maxWidth="xl">
+            <Grid container spacing={2}>
+                <Grid
+                    item
+                    xs={7}
+                    lg={7}
+                    xl={7}
+                    alignItems={"center"}
+                >
+                    <Grid container spacing={3}>
+                        {chairList.map((ghe) => {
+                            return (
+                                <Grid
+                                    item
+                                    key={ghe.tenGhe}
+                                    xs={3}
+                                    sm={2}
+                                    md={1}
+                                    lg={1}
+                                >
+                                    {
+                                        <Typography className="ghe" id={`ghe${ghe.loaiGhe}`}>
                                             {ghe.tenGhe}
                                         </Typography>
-                                    </Grid>
-                                );
-                            })}
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={5} lg={5}>
-                        <CardContent>
-                            <Stack
-                                spacing={2}
-                                divider={<Divider orientation="horizontal" flexItem />}
-                                direction="column"
-                            >
-                                <Stack
-                                    {...stackSettings}
-                                >
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        Tên phim:
-                                    </Typography>
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        <></>
-                                        {movieInfo.tenPhim}
-                                    </Typography>
-                                </Stack>
-                                <Stack
-                                    {...stackSettings}
-                                >
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        Cụm Rạp:
-                                    </Typography>
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        <></>
-                                        {movieInfo.tenCumRap}
-                                    </Typography>
-                                </Stack>
-                                <Stack
-                                    {...stackSettings}
-                                >
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        Rạp:
-                                    </Typography>
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        <></>
-                                        {movieInfo.tenRap}
-                                    </Typography>
-                                </Stack>
-                                <Stack
-                                    {...stackSettings}
-                                >
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        Ngày giờ chiếu:
-                                    </Typography>
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        <></>
-                                        {movieInfo.ngayChieu} {movieInfo.gioChieu}
-                                    </Typography>
-                                </Stack>
-                                <Stack
-                                    {...stackSettings}
-                                >
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        Địa chỉ:
-                                    </Typography>
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        <></>
-                                        {movieInfo.diaChi}
-                                    </Typography>
-                                </Stack>
-                                <Stack
-                                    {...stackSettings}
-                                >
-                                    <Typography id="gheDaDat" className="ghe"></Typography>
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        Ghế đã đặt</Typography>
-                                </Stack>
-                                <Stack
-                                    {...stackSettings}
-                                >
-                                    <Typography id="gheDangChon" className="ghe"></Typography>
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        Ghế đang chọn
-                                    </Typography>
-                                </Stack>
-                                <Stack
-                                    {...stackSettings}
-                                >
-                                    <Typography id="gheTrong" className="ghe"></Typography>
-                                    <Typography
-                                        {...typographySettings}
-                                    >
-                                        Ghế trống
-                                    </Typography>
-                                </Stack>
-                            </Stack>
-                            <Button
-                                variant="contained"
-                                style={{
-                                    // backgroundColor: "#ff3d00",
-                                    backgroundColor: `${red[500]}`,
-                                    display: "flex",
-                                    width: 150,
-                                    fontSize: 20,
-                                    margin: "auto",
-                                  }}
-                                onClick={() => {
-                                    // navigate(PATH.HOME);
-                                }}
-                            >Đặt vé
-                            </Button>
-                        </CardContent>
+                                    }
+
+                                </Grid>
+                            );
+                        })}
                     </Grid>
                 </Grid>
-            </Container>
-        </div >
+                <Grid item xs={5} lg={5}>
+                    <CardContent>
+                        <Stack
+                            spacing={2}
+                            divider={<Divider orientation="horizontal" flexItem />}
+                            direction="column"
+                        >
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Tên phim:
+                                </Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    <></>
+                                    {movieInfo.tenPhim}
+                                </Typography>
+                            </Stack>
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Cụm Rạp:
+                                </Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    <></>
+                                    {movieInfo.tenCumRap}
+                                </Typography>
+                            </Stack>
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Rạp:
+                                </Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    <></>
+                                    {movieInfo.tenRap}
+                                </Typography>
+                            </Stack>
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Ngày giờ chiếu:
+                                </Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    <></>
+                                    {movieInfo.ngayChieu} {movieInfo.gioChieu}
+                                </Typography>
+                            </Stack>
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Địa chỉ:
+                                </Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    <></>
+                                    {movieInfo.diaChi}
+                                </Typography>
+                            </Stack>
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography id="gheDaDat" className="ghe"></Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Ghế đã đặt</Typography>
+                            </Stack>
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography id="gheDangChon" className="ghe"></Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Ghế đang chọn
+                                </Typography>
+                            </Stack>
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography id="gheTrongThuong" className="ghe"></Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Ghế trống thường
+                                </Typography>
+                            </Stack>
+                            <Stack
+                                {...stackSettings}
+                            >
+                                <Typography id="gheTrongVIP" className="ghe"></Typography>
+                                <Typography
+                                    {...typographySettings}
+                                >
+                                    Ghế trống VIP
+                                </Typography>
+                            </Stack>
+                        </Stack>
+                        <Button
+                            variant="contained"
+                            style={{
+                                // backgroundColor: "#ff3d00",
+                                backgroundColor: `${red[500]}`,
+                                display: "flex",
+                                width: 150,
+                                fontSize: 20,
+                                margin: "auto",
+                            }}
+                            onClick={() => {
+                                // navigate(PATH.HOME);
+                            }}
+                        >Đặt vé
+                        </Button>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </Container>
     )
 }
 
