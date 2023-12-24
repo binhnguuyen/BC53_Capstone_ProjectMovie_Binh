ff4c8a3a94817269718d1b2cf24f0cb5303798f8
binhnguyen <leechiupin@gmail.com>
2023-12-24 11:35:43 +0900

    Sửa UI Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,10 +15,58 @@ import Typography from '@mui/joy/Typography';
 import Sheet from '@mui/joy/Sheet';
 import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import EmailIcon from '@mui/icons-material/Email';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import SendIcon from '@mui/icons-material/Send';
 import ColorLensRoundedIcon from '@mui/icons-material/ColorLensRounded';
+import { getTheaterSystemInfo, getTheaterInfo, getShowtimeInfo } from "../../apis/cinemaAPI";
+import { useQuery } from '@tanstack/react-query';
+import { Grid, Stack } from '@mui/material';
+import github from "../../assets/img/github.png"
+import MUI from "../../assets/img/MUI.png"
+import react from "../../assets/img/react.png"
+import slick from "../../assets/img/slick.png"
+import tanstackquery from "../../assets/img/tanstackquery.png"
+import HTML from "../../assets/img/HTML.png"
+import JS from "../../assets/img/JS.png"
+
 
 const Footer = () => {
+
+  const stackSettings = {
+    spacing: 2,
+    direction: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 50,
+  }
+
+  const typographySettings = {
+    // gutterbottom: true,
+    variant: "h6",
+    // marginBottom: 2,
+    style: {
+      fontSize: 20,
+      fontWeight: 700,
+    }
+  }
+
+  const iconButtonSettings = {
+    style: {
+      fontSize: 50,
+    }
+  }
+
+  const { data: theaterSystemData, isLoading: isLoadingTheaterSysytem } = useQuery({
+    queryKey: ["TheaterSystemInfo"],
+    // queryFn ko truyền vào tham số thì gọi thế này(nếu có truyền tham số thì phải gọi bằng 1 callback)
+    queryFn: getTheaterSystemInfo,
+  });
+  const theaterSystem = theaterSystemData || [];
+
   const [color, setColor] = React.useState('neutral');
   return (
     <Sheet
@@ -33,7 +81,7 @@ const Footer = () => {
         p: 2,
         borderRadius: { xs: 0, sm: 'sm' },
       }}
-    >
+      >
       <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
         <IconButton
           variant="soft"
@@ -49,10 +97,22 @@ const Footer = () => {
         </IconButton>
         <Divider orientation="vertical" />
         <IconButton variant="plain">
+          <GitHubIcon />
+        </IconButton>
+        <IconButton variant="plain">
           <FacebookRoundedIcon />
         </IconButton>
         <IconButton variant="plain">
-          <GitHubIcon />
+          <InstagramIcon />
+        </IconButton>
+        <IconButton variant="plain">
+          <TwitterIcon />
+        </IconButton>
+        <IconButton variant="plain">
+          <YouTubeIcon />
+        </IconButton>
+        <IconButton variant="plain">
+          <LinkedInIcon />
         </IconButton>
         <Input
           variant="soft"
@@ -92,20 +152,64 @@ const Footer = () => {
             minHeight={80}
             sx={{ flexBasis: { xs: 200, md: 'initial' } }}
           >
-            <img alt="" src="/static/blog/mui-product-comparison/ecosystem.png" />
+            <IconButton>
+              <img alt="" src={theaterSystem[5]?.logo} />
+            </IconButton>
           </AspectRatio>
           <CardContent>
-            <Typography level="body-sm">Intro to the MUI ecosystem</Typography>
-            <Typography level="body-xs">Blog post</Typography>
+            <Typography level="body-sm">Về chúng tôi</Typography>
           </CardContent>
         </Card>
+        <Stack>
+          <Stack {...stackSettings}>
+            <Typography {...typographySettings}>Đối tác </Typography>
+            {theaterSystem.map((item) => {
+              return (
+                <IconButton
+                  key={item.maHeThongRap}
+                  divider={<Divider orientation="vertical" flexItem />}>
+                  <>
+                    <img src={item.logo} style={{ width: 50 }} />
+                  </>
+                </IconButton>
+              );
+            })}
+          </Stack>
+          <Stack {...stackSettings} style={{ marginTop: 10 }}>
+            <Typography {...typographySettings}>Tài trợ </Typography>
+            <IconButton {...iconButtonSettings}>
+              {/* <i class="fab fa-github"></i> */}
+              <img src={github} alt="" style={{ width: 50, display: "inline-block", borderRadius: "50%" }} />
+            </IconButton>
+            <IconButton {...iconButtonSettings}>
+              <img src={HTML} alt="" style={{ width: 50, display: "inline-block", borderRadius: "50%" }} />
+              {/* <i class="fab fa-react"></i> */}
+            </IconButton>
+            <IconButton {...iconButtonSettings}>
+              <img src={JS} alt="" style={{ width: 50, display: "inline-block", borderRadius: "50%" }} />
+              {/* <i class="fab fa-java"></i> */}
+            </IconButton>
+            <IconButton {...iconButtonSettings}>
+              <img src={MUI} alt="" style={{ width: 50, display: "inline-block", borderRadius: "50%" }} />
+              {/* <i class="fab fa-html5"></i> */}
+            </IconButton>
+            <IconButton {...iconButtonSettings}>
+              <img src={react} alt="" style={{ width: 50, display: "inline-block", borderRadius: "50%" }} />
+              {/* <i class="fab fa-css3"></i> */}
+            </IconButton>
+            <IconButton {...iconButtonSettings}>
+              <img src={slick} alt="" style={{ width: 50, display: "inline-block", borderRadius: "50%" }} />
+              {/* <i class="fab fa-chrome"></i> */}
+            </IconButton>
+          </Stack>
+        </Stack>
         <List
           size="sm"
           orientation="horizontal"
           wrap
           sx={{ flexGrow: 0, '--ListItem-radius': '8px', '--ListItem-gap': '0px' }}
         >
-          <ListItem nested sx={{ width: { xs: '50%', md: 140 } }}>
+          <ListItem nested sx={{ width: { xs: '50%', md: 140 } }} {...typographySettings}>
             <ListSubheader sx={{ fontWeight: 'xl' }}>Sitemap</ListSubheader>
             <List>
               <ListItem>
@@ -119,7 +223,7 @@ const Footer = () => {
               </ListItem>
             </List>
           </ListItem>
-          <ListItem nested sx={{ width: { xs: '50%', md: 180 } }}>
+          <ListItem nested sx={{ width: { xs: '50%', md: 180 } }} {...typographySettings}>
             <ListSubheader sx={{ fontWeight: 'xl' }}>Products</ListSubheader>
             <List sx={{ '--ListItemDecorator-size': '32px' }}>
               <ListItem>
